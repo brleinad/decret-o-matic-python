e0f78b00335f671a29f15aef16378f39f6ebe5b9
brleinad <daniel.rodas.bautista@gmail.com>
2020-03-30 17:56:00 -0400

    Added initial display of valid decrees

--- decretomatic/game.py
+++ decretomatic/game.py
@@ -2,16 +2,16 @@ import time
 import pygame
 import os
 import random
+import pprint
 from pygame.locals import *
 
 WIDTH = 1000
 HEIGHT = 600
 SCALE_FACTOR = 1 #2.5 #for imagees that are too big
-#sprites = pygame.sprite.LayeredUpdates()
 GREEN = (0, 255, 0)
 TRANSPARENT = (1, 0, 0)
-
-
+MASK_POS_X = 0.35 #relative to WIDTH
+MASK_POS_Y = 0.4 #relative to HEIGHT
 
 
 class BaseSprite(pygame.sprite.Sprite):
@@ -50,74 +50,99 @@ class BaseSprite(pygame.sprite.Sprite):
             raise SystemExit(message)
         return sound
 
-#class Decreto():
-#    """
-#    """
-#    w1_dec = (
-#            "evitate",
-#            "chiuderemo",
-#            "non chiuderemo",
-#            "rimanete ne",
-#            "non rimanete ne",
-#            "recatevi ne",
-#            "non recatevi ne",
-#            )
-#    w2_dec = (
-#            "i parchi pubblici",
-#            "le scuole",
-#            "i cantieri",
-#            "il comune di residenza",
-#            "il luogo di lavoro",
-#            "la propria abitazione",
-#            "i supermercati",
-#            )
-#    w3_dec = (
-#            "per esigenze lavorative",
-#            "per beni di prima necessita'",
-#            "per produzioni di interesse strategico",
-#            "a un metro di distanza",
-#            "per motivi di salute",
-#            "in piu' di due persone",
-#            "per comprovate necessita'",
-#            )
-#    decreto = [[][][]]
-#
-#    def __init__(self):
-#
-#        i, j, k = 0, 0, 0
-#        for dec1 in w1_dec:
-#            for dec2 in w2_dec:
-#                for dec3 in w3_dec:
-#                    decreti[[i][j][k]] = ' '.join(dec1, dec2, dec3)
-#                    k += 1
-#                j += 1
-#            i += 1
-#                    
-#
-#
-#    def make_decreto(self, w_positions):
-#        pass
-#
-#    def get_decreto(self ):
-#        pass
-#
-#    def calc_decree_factor(self): 
-#        standard_factor = 2 + random.randint(0,10)*0.1
-#
-#        max_power = 10
-#        fix_power = 3
-#        exp_power = 1.2
-#        max_good_bad = 10
-#        good_bad_thresh = 6
-#
-#        rand_good_bad = random.randint(1, max_good_bad)
-#        rand_power = random.randint(1, max_power)
-#        factor_good_bad = -1 if rand_good_bad > good_bad_thresh else factor_good_bad = 1
-#        factor_power = fix_power // (random_power**exp_power)
-#
-#        factor = factor_good_bad * factor_power
-#
-#        return factor
+class Decrees():
+    """
+    Holds all the information used for managing decrees.
+    Just like a big smart dictionary.
+    """
+    w1_decs = (
+            "evitate",
+            "non recatevi ne",
+            "recatevi ne",
+            "non rimanete ne",
+            "rimanete ne",
+            "non chiuderemo",
+            "chiuderemo",
+            )
+    w2_decs = (
+            "i parchi pubblici",
+            "i supermercati",
+            "la propria abitazione",
+            "il luogo di lavoro",
+            "il comune di residenza",
+            "i cantieri",
+            "le scuole",
+            )
+    w3_decs = (
+            "per esigenze lavorative",
+            "per comprovate necessita'",
+            "in piu' di due persone",
+            "per motivi di salute",
+            "a un metro di distanza",
+            "per produzioni di interesse strategico",
+            "per beni di prima necessita'",
+            )
+
+    def __init__(self):
+
+        self.decrees = [[[' '.join((dec1, dec2, dec3)) for dec3 in self.w3_decs] for dec2 in self.w2_decs] for dec1 in self.w1_decs]
+
+        self.valid_decrees = [[['' for dec3 in self.w3_decs] for dec2 in self.w2_decs] for dec1 in self.w1_decs]
+
+        pprint.pprint(self.decrees)
+        pprint.pprint(self.valid_decrees)
+
+    def print_decs(self):
+        print(self.w1_decs)
+
+    def make_decreto(self, w_positions):
+        pass
+
+    def get_decreto(self):
+        pass
+
+    def add_valid_decree(self, decree_index):
+        """
+        Given a decree index touple it will add the corresponding decree to a list.
+        """
+        self.valid_decrees[decree_index[0]][decree_index[1]][decree_index[2]] = \
+            self.decrees[decree_index[0]][decree_index[1]][decree_index[2]]
+
+    def print_valid_decrees(self):
+        #pprint.pprint(self.valid_decrees)
+        for i in range(len(self.valid_decrees)):
+            for j in range(len(self.valid_decrees[i])):
+                for k in range(len(self.valid_decrees[j])):
+                    if self.valid_decrees[i][j][k]:
+                        print(self.valid_decrees[i][j][k])
+
+    def get_valid_decrees(self):
+        #pprint.pprint(self.valid_decrees)
+        valid_decs = []
+        for i in range(len(self.valid_decrees)):
+            for j in range(len(self.valid_decrees[i])):
+                for k in range(len(self.valid_decrees[j])):
+                    if self.valid_decrees[i][j][k]:
+                        valid_decs.append(self.valid_decrees[i][j][k])
+        return valid_decs
+
+    def calc_decree_factor(self): 
+        standard_factor = 2 + random.randint(0,10)*0.1
+
+        max_power = 10
+        fix_power = 3
+        exp_power = 1.2
+        max_good_bad = 10
+        good_bad_thresh = 6
+
+        rand_good_bad = random.randint(1, max_good_bad)
+        rand_power = random.randint(1, max_power)
+        factor_good_bad = -1 if (rand_good_bad > good_bad_thresh) else  1
+        factor_power = fix_power // (random_power**exp_power)
+
+        factor = factor_good_bad * factor_power
+
+        return factor
 #
 #
 #class People():
@@ -149,6 +174,7 @@ class Wheel(BaseSprite):
         self.spinning = 0
         self.spins = 0
         self.initial_image = self.image.copy()
+        self.decree_index = 0
 
     def update(self):
         """
@@ -156,6 +182,7 @@ class Wheel(BaseSprite):
         """
         if self.spinning:
             self._spin()
+        self.decree_index = self.spins
 
     def _spin(self):
         """
@@ -171,12 +198,11 @@ class Wheel(BaseSprite):
         if self.spins > 6:
             #self.image, self.rect = self.load_image(self.image_name, TRANSPARENT)
             self.image = self.initial_image.copy()
-            print('UBBI')
             self.spins = 0
         else:
             rotation_angle = 51.43 * self.spins + self.spinning
             self.image = pygame.transform.rotate(self.initial_image, rotation_angle)
-            print(self.spins)
+            #print(self.spins)
 
         self.rect = self.image.get_rect(center = center)
 
@@ -202,7 +228,7 @@ class Mask(BaseSprite):
         self.image = pygame.transform.scale(self.image, (int(self.size[0]//SCALE_FACTOR), int(self.size[1]//SCALE_FACTOR)))
         self.size = self.image.get_size()
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH*0.5, HEIGHT*0.5)
+        self.rect.center = (WIDTH*MASK_POS_X, HEIGHT*MASK_POS_Y)
         #sprites.add(self, layer=self.layer)
 
 
@@ -212,12 +238,6 @@ class Graph(BaseSprite):
     """
     pass
 
-
-class Decrees(BaseSprite):
-    """
-    A table to show all the decrees in action.
-    """
-    pass
 
 class Game:
     FLAGS = 0
@@ -234,11 +254,12 @@ class Game:
 
         self.background = pygame.Surface(self.screen.get_size())
         self.background = self.background.convert()
-        #self.background.fill((250, 250, 250))
-        #self.background.fill((0, 0, 0, 250))
+        #self.background.fill((250, 250, 250)) #WHITE
+        self.background.fill((0, 0, 0)) #BLACK
         self.background.set_colorkey((10,10,10,0))
         self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
+
 
         print(pygame.display.Info())
 
@@ -266,6 +287,14 @@ class Game:
         self.sprites.add(self.w1, layer = 3)
         self.sprites.add(self.w2, layer = 2)
         self.sprites.add(self.w3, layer = 1)
+
+        self.decrees = Decrees()
+        #decrees.print_decs()
+
+        self.title_font = pygame.font.SysFont('Comic Sans MS', 30, True)
+        self.decrees_font = pygame.font.SysFont('Comic Sans MS', 20)
+        textsurface = self.title_font.render('Decreti', False, (150, 150, 150))
+        self.screen.blit(textsurface,(WIDTH*0.8,HEIGHT*0.1))
         
     def events(self, events):
         """
@@ -282,14 +311,39 @@ class Game:
                     self.w2.next_decree()
                 elif event.key == K_3:
                     self.w3.next_decree()
-                #elif event.key == K_ENTER:
-                    #pass
+                elif event.key == K_SPACE:
+                    decree_index = (
+                            self.w1.decree_index, 
+                            self.w2.decree_index, 
+                            self.w3.decree_index)
+                    print('index')
+                    print(decree_index)
+                    self.decrees.add_valid_decree(decree_index)
+                    self.update_decrees_text()
+                elif event.key == K_p:
+                    self.decrees.print_valid_decrees()
 
 
     def render(self):
         """
         """
         pass
+
+    def update_decrees_text(self):
+        valid_decrees = 'Decreti\n'
+        #valid_decrees += self.decrees.get_valid_decrees_str()
+
+        textsurface = self.title_font.render('Decreti', False, (150, 150, 150))
+        text_x, text_y = WIDTH*0.6, HEIGHT*0.1
+        self.screen.blit(textsurface,(text_x, text_y))
+
+        word_width, word_height = textsurface.get_size()
+
+        for dec in self.decrees.get_valid_decrees():
+            dec_textsurface = self.decrees_font.render(dec, False, (150, 150, 150))
+            text_y += word_height
+            self.screen.blit(dec_textsurface,(text_x, text_y))
+
 
     def mainloop(self):
         """
@@ -306,6 +360,7 @@ class Game:
 
             self.sprites.update()
             self.screen.blit(self.background, (0, 0))
+            self.update_decrees_text()
             self.sprites.draw(self.screen)
             pygame.display.flip()
 
